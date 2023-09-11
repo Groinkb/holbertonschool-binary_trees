de82b4e98858e6475befdd74ac86ad7c961b1322
Groinkb <you@example.com>
2023-09-11 12:39:39 +0200

    Test Sarah le s

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,13 @@
+#include "binary_trees.h"
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	binary_tree_t *binary_tree_node(binary_tree_t * parent, int value);
+}
